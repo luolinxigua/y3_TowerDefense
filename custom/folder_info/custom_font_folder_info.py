6e39fd1d75bce00a60671a474d9e8c01e150a594
luolinxigua <luolin990414@126.com>
2024-06-03 00:08:30 +0800

    first commit

---
+++ custom/folder_info/custom_font_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {134280517: (2147483647, 0), 134228746: (2147483647, 1), 134266569: (2147483647, 2)}}
